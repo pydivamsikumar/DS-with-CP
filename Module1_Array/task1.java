a0a4746189207a1ce1d9efb088f099072a202242
pydivamsikumar <pydivamsikumar264@gmail.com>
2025-12-20 15:31:20 +0530

    Use loop to print array elements
    Refactor code to use a loop for printing array elements.

--- Module1_Array/task1.java
+++ Module1_Array/task1.java
@@ -1,1 +1,19 @@
-code
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        int[] n={10,20,30,40};
+        System.out.println(n[0]); //zeroth index -> 1st element of array
+        System.out.println(n[1]);
+        System.out.println(n[2]);
+        System.out.println(n[3]);
+        System.out.println (n.length+"total elements in array");// total elements in array
+        
+        //why we are repeating ourselves 
+        for(int i=0;i<n.length;i++){
+         System.out.println("index " +i+" element is"+n[i]); 
+        }
+    }
+}code

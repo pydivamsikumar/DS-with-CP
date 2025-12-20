bc0d039835882886b2552755c9ebf263d96165d8
pydivamsikumar <pydivamsikumar264@gmail.com>
2025-12-20 16:03:47 +0530

    Refactor array handling and add dynamic allocation
    Refactored array printing logic and added dynamic allocation examples.

--- Module1_Array/task1.java
+++ Module1_Array/task1.java
@@ -1,19 +1,40 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
-
 class Main {
     public static void main(String[] args) {
-        System.out.println("Try programiz.pro");
-        int[] n={10,20,30,40};
-        System.out.println(n[0]); //zeroth index -> 1st element of array
+        
+        // static D& I
+        int[]n={10,20,30,40};
+        System.out.println(n[0]);
         System.out.println(n[1]);
         System.out.println(n[2]);
         System.out.println(n[3]);
-        System.out.println (n.length+"total elements in array");// total elements in array
+        System.out.println(n.length);
         
-        //why we are repeating ourselves 
-        for(int i=0;i<n.length;i++){
-         System.out.println("index " +i+" element is"+n[i]); 
+        for(int i=0; i<n.length; i++){
+            System.out.println("at index "+ i + " is " + n[i]);
         }
+        // static D& I we are using new key word
+        int[] n1= new int[] {1,2,3,4};
+        
+        for(int i=0; i<n1.length; i++){
+            System.out.println("at index "+ i + " is " + n1[i]);
+        }
+        
+        String[] names =new String[] {"jeet","raj","ram"};
+        for(int i=0; i<names.length; i++){
+            System.out.println("at index "+ i + " is " + names[i]);
+        }
+        
+        // Dynamic Allocation
+        int[] n3= new int[10];
+        n3[0]=200;
+        n3[9]=2000;
+        
+        for(int i=0; i<n3.length; i++){
+            System.out.println("at index "+ i + " is " + n3[i]);
+        }
+        
+        
+        
+        
     }
 }

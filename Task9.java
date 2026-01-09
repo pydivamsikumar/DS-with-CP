df3c3cae248cbfa5499ab34bcb491f95ad4cc6f7
pydivamsikumar <pydivamsikumar264@gmail.com>
2026-01-09 15:04:48 +0530

    Implement Node class with basic functionality

---
+++ Task9.java
@@ -0,0 +1,32 @@
+
+class Node{
+   private int data;
+    Node next;
+    Node(int a){
+        data=a;
+        next=null;
+        
+    }
+    public void getdata(){
+        System.out.println(data);
+    }
+    public void getref(){
+        System.out.println(next);
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        System.out.println("vamsi kumar");
+        Node a =new Node(100);
+        
+        Node  b=new Node(200);
+        a.getdata();
+        a.getref();
+        
+        a.next=b;
+        
+        a.getref();
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
